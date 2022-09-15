811b9b3f5b4088ac821daa81d2faa109d2e89ed8
Onishola <onisholaoluwatobiloba@gmail.com>
2022-09-15 10:26:04 -0700

    TASK 0 corrected now

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,16 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/*
- * file - main.h
- * Author: Onishola Oluwatobiloba
- * Description: Header contains prototypes for all funtions
- * used in 0x03-more_functions_nested_loops_directory
- */
+#include <stdio.h>
 
-int _putchar(char c);
-int _isdigit(int c);
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
 
-
-
-#endif /* MAIN_H */
+#endif /* MAN_H */
