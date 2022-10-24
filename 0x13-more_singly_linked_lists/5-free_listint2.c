5d6a02778d66da715682c307b7a0d80a51f91dc8
Onishola <onisholaoluwatobiloba@gmail.com>
2022-10-24 13:04:15 -0700

    TASK 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function entry
+ * @head: ptr to ptr
+ * Desc: frees a list
+ *
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+
+	listint_t *freed;
+
+
+
+	if (head == NULL)
+
+		return;
+
+	while (*head)
+
+	{
+
+		freed = (*head)->next;
+
+		free(*head);
+
+		*head = freed;
+
+	}
+}
